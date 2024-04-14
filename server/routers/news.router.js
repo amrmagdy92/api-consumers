7aadeadc8c210d8235de51b5644203853aae9f85
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-04-14 18:12:50 +0200

    Consumed the controller to create the news router

---
+++ server/routers/news.router.js
@@ -0,0 +1,38 @@
+import { Router } from "express"
+import { fetchFromOKSurf } from "../controllers/news.controller"
+
+const router = Router()
+
+router.route("/")
+    .get((request, response) => {
+        if (request.query.endPointName === undefined) {
+            response.status(400).json({ msg: "Missing endpoint name" })
+        } else {
+            fetchFromOKSurf(request.query.endPointName)
+            .then( result => {
+                let responseCode = result.code
+                let message = result.msg
+                response.status(responseCode).json({ msg: message })
+            })
+            .catch(err => {
+                let responseCode = err.code
+                let resultData = err.msg
+                response.status(responseCode).json({ msg: resultData })
+            })
+        }
+    })
+    .post((request, response) => {
+        fetchFromOKSurf(request.query.endPointName, request.body)
+        .then( result => {
+            let responseCode = result.code
+            let message = result.msg
+            response.status(responseCode).json({ msg: message })
+        })
+        .catch(err => {
+            let responseCode = err.code
+            let resultData = err.msg
+            response.status(responseCode).json({ msg: resultData })
+        })
+    })
+
+export default router
