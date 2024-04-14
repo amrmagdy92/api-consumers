2203de23aed847286a31e624337c5cc5e1e7e77e
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-04-14 16:37:38 +0200

    created base app file

---
+++ server/app.js
@@ -0,0 +1,58 @@
+// Base Module
+import express from "express"
+
+// Security Modules
+import cors from "cors"
+import helmet from "helmet"
+import expressRateLimiter from "express-rate-limit"
+
+// Environment configuration
+import dotenv from "dotenv"
+
+// Request handlers
+import bodyParser from "body-parser"
+import compress from "compression"
+
+// Routers
+
+// app configuration
+dotenv.config()
+const configuredBodyParserJSON = bodyParser.json()
+const configuredBodyParserURLEncoding = bodyParser.urlencoded({ extended: true })   
+const configuredCompress = compress()
+
+// Security Configurations
+const configuredHelmet = helmet({ crossOriginResourcePolicy: false })
+const configuredCors = cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: process.env.CORS_METHODS,
+    credentials: process.env.CORS_CREDENTIALS
+})
+const configuredRateLimiter = expressRateLimiter({
+    window: process.env.RATE_WINDOW,
+    max: process.env.RATE_MAX,
+    message: process.env.RATE_MESSAGE,
+    Headers: true,
+    keyGenerator: (req, res) => {
+        return req.ip
+    }
+})
+
+// App initialization
+const app = express()
+app.use(configuredBodyParserJSON)
+app.use(configuredBodyParserURLEncoding)
+app.use(configuredCompress)
+app.use(configuredHelmet)
+app.use(configuredCors)
+app.use(configuredRateLimiter)
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.send("hello")
+})
+
+// Routes
+
+// Export
+export default app
