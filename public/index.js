8fc37a0991d40aed588c876e9d9b25673bcf6def
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-04-15 15:30:29 +0200

    Implemented API list requester

--- public/index.js
+++ public/index.js
@@ -36,7 +36,24 @@ function requestNews(category) {
     request.send(JSON.stringify(body))
 }
 
+function requestAPIList() {
+    const request = new XMLHttpRequest()
+    request.open('GET', 'http://localhost:3000/api/v1/apilist')
+    request.setRequestHeader('Content-Type', 'application/json')
+    request.setRequestHeader('Accept', 'application/json')
+    request.setRequestHeader('Access-Control-Allow-Origin', '*')
+    request.addEventListener('load', function() {
+        if (request.status === 200 && request.readyState === 4) {
+            document.getElementById('api-nav').innerHTML = request.response
+        } else {
+            console.log(request.response)
+        }
+    })
+    request.send()
+}
+
 window.onload = function () {
     requestNewsCategories()
+    requestAPIList()
     requestNews("Technology")
 }
