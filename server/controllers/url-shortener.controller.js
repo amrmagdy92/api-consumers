9280c75ff4ff2451f3a0382c7c9f5d53193b216c
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-04-15 16:36:59 +0200

    fixed body passed to fetch function

--- server/controllers/url-shortener.controller.js
+++ server/controllers/url-shortener.controller.js
@@ -1,25 +1,26 @@
 const shortenURL = (url) => {
     return new Promise((resolve, reject) => {
+        console.log(url)
         fetch('https://cleanuri.com/api/v1/shorten', {
             method: 'POST',
-            body: url,
+            body: JSON.stringify({ url: url}),
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-            .then(response => response.json())
-            .then(data => {
-                resolve({
-                    code: 200,
-                    msg: data
-                })
+        .then(response => response.json())
+        .then(data => {
+            resolve({
+                code: 200,
+                msg: data
             })
-            .catch(err => {
-                reject({
-                    code: 500,
-                    msg: err
-                })
+        })
+        .catch(err => {
+            reject({
+                code: 500,
+                msg: err
             })
+        })
     })
 }
 
