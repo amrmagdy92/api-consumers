e19d526fed1f9a5953ddb6c11a4bee228356e9b4
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-04-17 11:32:15 +0200

    Implemented the get picture of the day function

--- server/controllers/nasa.controller.js
+++ server/controllers/nasa.controller.js
@@ -1,4 +1,29 @@
-const getPOD = (queries) => {}
+import { validateQuery } from "../helpers/nasa.helpers"
+
+const getPOD = (queries) => {
+    let NASAURL = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`
+    let validationErrors = validateQuery(queries)
+    if (Object.keys(validationErrors).length > 0) {
+        reject({
+            code: 400,
+            msg: `Invalid query: ${validationErrors}`
+        })
+    } else {
+        if (Object.keys(queries).length > 0) {
+            for (let i=0; i<Obbject.keys(queries).length; i++) {
+                NASAURL += `&${Object.keys(queries)[i]}=${Object.values(queries)[i]}`
+            }
+        }
+        fetch(NASAURL)
+        .then(response => response.json())
+        .then(data => {
+            resolve({
+                code: 200,
+                msg: data
+            })
+        })
+    }
+}
 const getEPIC = () => {}
 const getTLE = () => {}
 
