6bd5b485277fd1e5f61f83289799266046e14214
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-04-17 10:43:46 +0200

    Added base files for the dune api

---
+++ server/controllers/dune.controller.js
@@ -0,0 +1,11 @@
+const getRandomQuote = () => {}
+const getManyQuotes = (quotesCount) => {}
+const getRandomBook = () => {}
+const getManyBooks = (booksCount) => {}
+
+export {
+    getRandomQuote,
+    getManyQuotes,
+    getRandomBook,
+    getManyBooks
+}
