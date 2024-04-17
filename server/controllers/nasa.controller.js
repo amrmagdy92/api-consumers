e6fc4793907171f81425c9924b21bf739a1ba70b
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-04-17 19:18:54 +0200

    removed the validator and the passed queries variable to simplyfy the api consumption

--- server/controllers/nasa.controller.js
+++ server/controllers/nasa.controller.js
@@ -1,29 +1,20 @@
-import { validateQuery } from "../helpers/nasa.helpers"
-
-const getPOD = (queries) => {
+const getPOD = () => {
     return new Promise((resolve, reject) => {
         let NASAURL = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`
-        let validationErrors = validateQuery(queries)
-        if (Object.keys(validationErrors).length > 0) {
+        fetch(NASAURL)
+        .then(response => response.json())
+        .then(data => {
+            resolve({
+                code: 200,
+                msg: data
+            })
+        })
+        .catch(err => {
             reject({
-                code: 400,
-                msg: `Invalid query: ${validationErrors}`
+                code: 500,
+                msg: err
             })
-        } else {
-            if (Object.keys(queries).length > 0) {
-                for (let i=0; i<Object.keys(queries).length; i++) {
-                    NASAURL += `&${Object.keys(queries)[i]}=${Object.values(queries)[i]}`
-                }
-            }
-            fetch(NASAURL)
-            .then(response => response.json())
-            .then(data => {
-                resolve({
-                    code: 200,
-                    msg: data
-                })
-            })
-        }
+        })
     })
 }
 
